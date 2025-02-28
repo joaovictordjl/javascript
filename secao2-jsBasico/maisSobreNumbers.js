7cf6ea1f441644959dd08fdb1019aa5ab4a6338e
João Victor <victorjhon779@gmail.com>
2025-02-27 23:11:17 -0300

    commit

---
+++ secao2-jsBasico/maisSobreNumbers.js
@@ -0,0 +1,17 @@
+    let num1 = 1;
+    let num2 = 2.5;
+
+    //como unir os dois numbers como se fossem strings??? 
+    console.log(num1.toString() + num2);
+
+    //como tornar o number em binário
+    let num3 = 10;
+    console.log(num3.toString(2));
+
+    //como escolher o tanto de casas decimais que quer na saída
+    let num4 = 50.1234567;
+    console.log(num4.toFixed(3)); //3 casas decimais
+
+    //verifica se o número é inteiro
+    let num5 = 15;
+    console.log(Number.isInteger(num5));
