9c3d9870a8965e6a25ed4f75dc187a6b6a3aded4
João Victor <victorjhon779@gmail.com>
2025-03-09 19:43:22 -0300

    hihihi

--- LÓGICA-JS/IF ELSE/index.js
+++ LÓGICA-JS/IF ELSE/index.js
@@ -1,2 +1,19 @@
-const hora = 10;
+// Entre 0 e 12hrs
+// Entre 12 e 19
+// Entre 19h e 00
+
+//IF PODE SER EXECUTADO SOZINHO
+//ELSE PRECISA DE UM IF ANTES
+//PODE TER VÁRIOS ELSE IFS 
+//SÓ PODE TER UM ELSE
+
+const hora = 23;
+
+if (hora >= 0 && hora <= 12) {
+    console.log(`Bom dia, ja são ${hora} horas`);
+} else if (hora > 12 && hora <=18) {
+    console.log(`Boa Tarde, ja são ${hora} horas`);
+} else {
+    console.log(`Boa noite, já são ${hora} horas`);
+}
 
